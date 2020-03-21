f5f7bfb67a69bafcb79b538b1cf40fe68fa570b4
Julio Sanjuan <sahj@live.com.mx>
2020-03-21 13:14:31 -0600

    Nueva interfaz de lista de documentos
    Se agrega bootstrap y funcionalidad agregar documento

--- javascript/prototipos/documento.js
+++ javascript/prototipos/documento.js
@@ -1,11 +1,12 @@
-var Documento = function(){
-    var nombre;
+var Documento = function(nom_doc){
+    var nombre = nom_doc;
     var archivos = [];
-    this.guardar
-    this.cargar
-    this.reemplazar
     this.getNombre = function(){
         return nombre;
+    }
+
+    this.setNombre = function(nvo_nombre){
+        nombre = nvo_nombre;
     }
 
     function mostrar(){
